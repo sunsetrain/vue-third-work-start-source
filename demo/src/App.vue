3c93f09333d832a02fac2df722523d249988b8fa
Mikhail Bezzubikov <spit1ire@yandex.ru>
2024-04-15 10:37:39 +0300

    next 1.35. Компонент: стили

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <router-view />
+  <app-layout>
+    <home-view />
+  </app-layout>
 </template>
 
-<style>
-* {
-  padding: 0;
-  margin: 0;
-  font-family: sans-serif;
-}
+<script setup>
+import { AppLayout } from "@/layouts";
+import { HomeView } from "@/views";
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/app.scss";
 </style>
